c0bbd707bd86495a6c8a3170609a219ef8544161
sandipdgit <sandipdhumal@gmail.com>
2025-03-10 21:02:28 +0530

    Identity based configuration URL added

--- CoreWebBulky/Program.cs
+++ CoreWebBulky/Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+//Should be add after addIdentity
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 //builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddRazorPages();
